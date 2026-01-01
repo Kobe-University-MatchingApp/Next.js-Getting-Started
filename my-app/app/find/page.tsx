d90382174977caebcba47d6c363f60afdb7f27af
NAKANO.M <114970041+stumn@users.noreply.github.com>
2026-01-01 22:36:52 +0900

    supabase.tsファイルを削除

--- my-app/app/find/page.tsx
+++ my-app/app/find/page.tsx
@@ -1,4 +1,4 @@
-import { supabase } from '@/lib/supabase';
+import { supabase } from '@/lib/supabaseClient';
 import { Event } from '@/types/event';
 import FindEventsClient from './FindEventsClient';
 
